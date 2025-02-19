f8dc87a06d0a598dc24457946b1e849c7277b349
darkpixelbot <89727925+darkpixelbot@users.noreply.github.com>
2025-02-19 19:00:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOKEN = "7439987936:AAEEOSQ2l4MSEejLizANp6SXURkNcpqGCMU"
 API_ID = 26918101  # Replace with your API ID from my.telegram.org
 API_HASH = "57d6680f6549e21aca4e93c7a4221d29"  # Replace with your API Hash
-MESSAGE_THRESHOLD = 5  # Number of messages before a Pokémon appears
+DEFAULT_THRESHOLD = 100  # Default drop time for all groups
 BOT_OWNER_ID = 996392648
